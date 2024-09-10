d4130d5c7a2b55c6e22367344fbe7513e55ab469
BazZziliuS <bazzzil123@gmail.com>
2024-09-10 20:23:37 +0300

    Init repo

---
+++ docusaurus.config.js
@@ -0,0 +1,121 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+const lightCodeTheme = require('prism-react-renderer').themes.nightOwlLight;
+const darkCodeTheme = require('prism-react-renderer').themes.nightOwl;
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Блог BazZziliuS',
+  tagline: 'BazZziliuS',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://your-docusaurus-site.example.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'BazZziliuS', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'throw',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'ru',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: "/docs",
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl: 'https://github.com/BazZziliuS/docusaurus/tree/main/',
+          showLastUpdateTime: true,
+        },
+        blog: {
+          routeBasePath: '/',
+          showReadingTime: true,
+          blogSidebarCount: 'ALL',
+          blogSidebarTitle: 'Другие посты',
+        },
+
+        theme: {
+          customCss: [
+            // not my styles. Taken from here:
+            // https://github.com/vendure-ecommerce/vendure/blob/cc4826dfb7c1a2f4e6ed8daa13eb017090d8bd9a/docs/src/css/custom.css
+            require.resolve('./src/css/custom.css'),
+            require.resolve('./src/css/layout.css'),
+            require.resolve('./src/css/overrides.css'),
+            // require.resolve('./src/css/code-blocks.css'),
+          ],
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: '',
+        logo: {
+          alt: 'Лого',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'dropdown',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: '✍️ Блог',
+            items: [
+              {label: 'Главная', to: '/'},
+              {label: 'Все по годам', to: 'archive'},
+            ],
+          },
+          {
+            type: 'dropdown',
+            label: '🙃 Не блог',
+            position: 'left',
+            items: [
+              {label: '📦 Куча полезностей', type: 'doc', docId: 'intro'},
+              {label: '👤 Обо мне', to: 'about'},
+              {label: '✈️ TG Боты', to: 'docs/telegram/bots'},
+            ],
+          },
+          {
+            href: 'https://github.com/BazZziliuS/docusaurus',
+            position: 'right',
+            className: "header-github-link",
+            "aria-label": "GitHub repository",
+          },
+        ],
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+        additionalLanguages: ['python', 'bash', 'lua'],
+      },
+			algolia: {
+				appId: "QEC31FXAWB",
+				apiKey: "5cd6537db1f15927e3df9a683a50642d",
+				indexName: "bazzzilius",
+			},
+    }),
+};
+
+export default config;
---
+++ src/pages/about.md
@@ -0,0 +1,25 @@
+---
+title: Обо мне
+slug: about
+image: https://centurymine.net/templates/Default/assets/image/logo.png
+---
+
+### 🙋‍♂️ About Me 
+
+- 🔭 Hello My name is BazZziliuS!
+- 🌱 II'm currently surviving as best I can.
+- 👯 I am interested in Python and some other interests & interesting things!
+- ⚡ Fun fact, I love strangling python.
+
+
+
+### 🧰 PC config
+![icon](https://img.shields.io/badge/NVIDIA-RTX4060-76B900?style=for-the-badge&logo=nvidia&logoColor=white)
+![icon](https://img.shields.io/badge/Intel-Core_i7_11th-0071C5?style=for-the-badge&logo=intel&logoColor=white)
+![icon](https://img.shields.io/badge/RAM-32GB_3200MHz-0078D6?style=for-the-badge&logo=adata&logoColor=white)
+![icon](https://img.shields.io/badge/Windows-11_TINY-0078D6?style=for-the-badge&logo=windows&logoColor=white)
+![icon](https://img.shields.io/badge/SSD_M.2-512GB_Ally_AL1284-d4dcd3?style=for-the-badge&logo=ardor&logoColor=white)
+![icon](https://img.shields.io/badge/SSD-240GB_A400-d4dcd3?style=for-the-badge&logo=kingstontechnology&logoColor=white)
+![icon](https://img.shields.io/badge/HDD-2TB_ST2000DM001-d4dcd3?style=for-the-badge&logo=seagate&logoColor=white)
+
+
